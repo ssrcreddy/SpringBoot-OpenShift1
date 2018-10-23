e5d92f28fd7a7d3c120aa0bafc2c1be1c1056d07
ssrcreddy <ssrcreddy@gmail.com>
2018-10-22 22:54:22 -0400

    Add files via upload

---
+++ src/main/java/com/example/demo/Proj1Application.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class Proj1Application {
+	
+	public String welcome() {
+		return "Test application for OpenShift";
+	}
+	
+	@GetMapping("/{name}")
+	public String sayHello(@PathVariable String name) {
+		return "Hello"+name;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj1Application.class, args);
+	}
+}
